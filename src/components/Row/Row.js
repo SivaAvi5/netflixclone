589c0b38c30375137b0f9a86a2883c906e7d0d34
SivaAvi5 <avissiva229@gmail.com>
2024-06-25 17:44:42 +0530

    feat:add youtube trailer

--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -1,47 +1,47 @@
 import React, { useEffect, useState } from "react";
 import "./Row.css";
 import axios from "../../helpers/axios";
-// import YouTube from "youtube-react";
-// import movieTrailer from "movie-trailer";
+import YouTube from "react-youtube";
 
 const Row = ({ title, fetchUrl, isLarge = false }) => {
   const [movies, setMovies] = useState([]);
-  // const [trailerUrl, setTrailerUrl] = useState("");
+  const [trailerUrl, setTrailerUrl] = useState("");
+  const YOUTUBE_API_KEY = "AIzaSyC18wu8GFtrqNVOqQq_tg9IxGKBMppJS1I"; // Replace with your YouTube API key
 
   const base_url = "https://image.tmdb.org/t/p/original/";
 
   useEffect(() => {
     const fetchData = async () => {
       const request = await axios.get(fetchUrl);
-      console.log(isLarge, request);
       setMovies(request.data.results);
       return request;
     };
     fetchData();
-  }, [fetchUrl,isLarge]);
+  }, [fetchUrl]);
 
-  // const opts = {
-  //   height: "390",
-  //   width: "100%",
-  //   playerVars: {
-  //     autoplay: 1,
-  //   },
-  // };
+  const handleClick = async (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      try {
+        const response = await axios.get(
+          `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${movie.name} trailer&type=video&key=${YOUTUBE_API_KEY}`
+        );
+        const videoId = response.data.items[0]?.id?.videoId;
+        setTrailerUrl(videoId);
+      } catch (error) {
+        console.error(`Error fetching trailer: ${error.message}`);
+      }
+    }
+  };
 
-  // const handleClick = (movie) => {
-  //   if (trailerUrl) {
-  //     setTrailerUrl("");
-  //   } else {
-  //     movieTrailer(movie?.name || "")
-  //       .then((url) => {
-  //         const urlParams = new URLSearchParams(new URL(url).search);
-  //         setTrailerUrl(urlParams.get("v"));
-  //       })
-  //       .catch((error) => {
-  //         console.log(error);
-  //       });
-  //   }
-  // };
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
 
   return (
     <div className="row">
@@ -51,17 +51,16 @@ const Row = ({ title, fetchUrl, isLarge = false }) => {
           <img
             key={movie.id}
             className={`row__poster ${isLarge && "row__posterLarge"}`}
-            src={`${base_url}${
-              isLarge ? movie.poster_path : movie.poster_path
-            }`}
+            src={`${base_url}${movie.poster_path}`}
             alt={movie.name}
-            // onClick={() => handleClick(movie)}
+            onClick={() => handleClick(movie)}
           />
         ))}
-        {/* {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />} */}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 };
 
 export default Row;
+
